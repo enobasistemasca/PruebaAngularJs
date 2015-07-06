81e8db87e131059afe6bb3c303b7db156aeb7d8d
Danny Goncalves <dgoncalves@enobasistemas.com>
2015-07-06 17:14:53 -0430

    Subiendo la completación del branch 'Login' sin pruebas unitarias

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,8 +1,71 @@
-var loginModule = angular.module('loginModule', []);
+(function() {
+  'use strict';
 
-peopleModule.controller('LoginController', [
-	'$scope',
-	'$http',
-	function($scope, $http){
-	} 
-]);
+  angular
+    .module('pruebaangularjs')
+    .controller('LoginController', LoginController);
+
+  /** @ngInject */
+  // Login Controller
+  function LoginController($scope, $http, toastr, $location, $timeout) {
+      $scope.user = {};
+      $scope.forgot = {};
+
+      $scope.loginForm = function(){
+        $http.put('http://www.enobashop.com/angularservices/login.php', $scope.user)
+
+        .success(function(data){
+          if(data === 0){
+            toastr.error('El nombre de usuario no existe', 'Error');
+          }else if(data === 2){
+            toastr.warning('La contraseña no coincide', 'Error');
+          }else{
+            toastr.success('Sesión iniciada correctamente', '¡Éxito!');
+            // Timeout and redirect
+            var countUp = function() {
+                $location.path( "/home" );
+            }
+            $timeout(countUp, 500)
+          }
+        })
+        .error(function(data, code){
+          alert(code);
+        });
+      };
+
+      $scope.forgotForm = function(){
+        $http.put('http://www.enobashop.com/angularservices/forgot.php', $scope.forgot)
+
+        .success(function(data){
+          if(data === 0){
+            toastr.error('El nombre de usuario no existe', 'Error');
+          }else if(data === 1){
+            toastr.success('La nueva contraseña se ha enviado al correo electrónico', '¡Éxito!');
+          }
+        })
+        .error(function(data, code){
+          alert(code);
+        });
+      };
+  }
+
+  // Forgot Controller
+/*  function ForgotController($scope, $http, toastr) {
+      $scope.forgot = {};
+
+      $scope.forgotForm = function(){
+        $http.put('http://www.enobashop.com/angularservices/forgot.php', $scope.forgot)
+
+        .success(function(data){
+          if(data === 0){
+            toastr.error('El nombre de usuario no existe', 'Error');
+          }else{
+            toastr.success('La nueva contraseña se ha enviado al correo electrónico', '¡Éxito!');
+          }
+        })
+        .error(function(data, code){
+          alert(code);
+        });
+      };
+  }*/
+})();
