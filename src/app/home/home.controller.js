e2be1c5af61a603cca2fa0ae17f5d48692873bb9
fpernia <fpernia@enobasistemas.com>
2015-07-08 16:06:34 -0430

    Delete finalizado

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -6,9 +6,9 @@
     .controller('HomeController', HomeController);
 
   /** @ngInject */
-  function HomeController($scope, $http) {
+  function HomeController($scope, $http, toastr, $location, $timeout) {
     $scope.busqueda = "";
-    $scope.users="";
+    $scope.resultados="";
     $scope.filtro="";
 
     $http.get('http://www.enobashop.com/angularservices/fetch.php').
@@ -21,5 +21,21 @@
       // or server returns response with an error status.
     });
 
+    $scope.eliminar = function(usuario){
+
+      $http.get('http://www.enobashop.com/angularservices/delete_customer.php?id='+usuario.id).
+      success(function(data, status, headers, config) {
+        if(data){
+          $scope.resultados.splice( $scope.resultados.indexOf(usuario), 1 );
+          toastr.success('Usuario eliminado', '¡Éxito!');          
+        }else{
+          toastr.error('El usuario no pudo ser eliminado', 'Error');
+        }
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+    }
   }
 })();
